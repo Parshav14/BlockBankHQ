fa9fdc01de9f445447465ce5cd0bbc3f47910cbc
Parshav14 <Parshav769@gmail.com>
2024-07-18 00:38:23 +0530

    my bank created

--- myBank/BBHQ.sol
+++ myBank/BBHQ.sol
@@ -7,7 +7,38 @@ contract myBank {
         uint256 balance;
         uint256 accountCreationTime;
     }
-    mapping(address => uint256) public BlockBankHQ;
-    
+    mapping(address => Account) public BlockBankHQ;
+
+    event balanceAdded(address owner, uint256 amount, uint256 timestamp);
+    event withdrawlAmount(address owner, uint256 amount, uint256 timestamp);
+
+    modifier minimum () {
+        require(msg.value >= 1 ether, "Please add the minimum funds for account creation");
+        _;
+    }
+
+    function accountCreated()
+        public payable minimum {
+            BlockBankHQ[msg.sender].owner = msg.sender;
+            BlockBankHQ[msg.sender].balance = msg.value;
+            BlockBankHQ[msg.sender].accountCreationTime = block.timestamp;
+
+            emit balanceAdded(msg.sender, msg.value, block.timestamp);
+        }
+
+    function depositAmount()
+       public payable minimum {
+        BlockBankHQ[msg.sender].balance += msg.value;
+
+        emit balanceAdded(msg.sender, msg.value, block.timestamp);
+       }
+
+    function withdrawMoney()
+    public payable {
+        // require(msg.value >= 10 ether && msg.value <= 1000 ether, "Please enter the value in between it.");
+        payable(msg.sender).transfer(BlockBankHQ[msg.sender].balance);
+        BlockBankHQ[msg.sender].balance = 0;
+        emit withdrawlAmount(msg.sender, BlockBankHQ[msg.sender].balance, block.timestamp);
+    }
 
 }
