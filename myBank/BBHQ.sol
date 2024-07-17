15f16ddada817e9884f68a091c660a4fb97b8b3f
Parshav14 <Parshav769@gmail.com>
2024-07-17 23:30:56 +0530

    New commit

---
+++ myBank/BBHQ.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract myBank {
+    struct Account {
+        address owner;
+        uint256 balance;
+        uint256 accountCreationTime;
+    }
+    mapping(address => uint256) public BlockBankHQ;
+
+    
+}
